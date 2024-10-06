9c906475c1c1c56c85449c94fc3c4e5bebd72cba
nazar <nazar@MOLOTOK>
2024-10-06 18:20:02 +0300

    Add project files.

---
+++ Data/ModelSeeder.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StoreApp.Models;
+public static class ModelSeeder
+{
+    public static void Seed(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Country>().HasData(
+            new Country { Id = 1, Name = "USA" },
+            new Country { Id = 2, Name = "Canada" }
+        );
+        modelBuilder.Entity<City>().HasData(
+            new City { Id = 1, Name = "New York", CountryId = 1 },
+            new City { Id = 2, Name = "Toronto", CountryId = 2 }
+        );
+        modelBuilder.Entity<Position>().HasData(
+            new Position { Id = 1, Name = "Manager" },
+            new Position { Id = 2, Name = "Salesperson" }
+        );
+        modelBuilder.Entity<Category>().HasData(
+            new Category { Id = 1, Name = "Electronics" },
+            new Category { Id = 2, Name = "Clothing" }
+        );
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using StoreApp.Data;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var services = new ServiceCollection();
+        services.AddDbContext<StoreDbContext>(options =>options.UseSqlServer("workstation id=hattoriq1.mssql.somee.com;packet size=4096;\r\n                                        user id=hattori_SQLLogin_1;\r\n                                        pwd=9s2l522m2p;\r\n                                        data source=hattoriq1.mssql.somee.com;persist security info=False;\r\n                                        initial catalog=hattoriq1;\r\n                                        TrustServerCertificate=True"));
+        var provider = services.BuildServiceProvider();
+        var context = provider.GetRequiredService<StoreDbContext>();
+        context.Database.Migrate();
+        Console.WriteLine("Database setup complete.");
+    }
+}
